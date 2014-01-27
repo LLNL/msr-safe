2b15f30356d7929093cb7e284c647859727bd388
Barry Rountree <rountree@llnl.gov>
2014-01-26 20:27:19 -0800

    Adding in the lseek code.
    Compiles on haswell.  Committing to test on cab.

--- msr_safe.c
+++ msr_safe.c
@@ -89,12 +89,26 @@ static long msr_ioctl(struct file *file, unsigned int ioc, unsigned long arg)
 
 static loff_t msr_seek(struct file *file, loff_t offset, int orig)
 {
-	// seek doesn't make sense for registers.
-	(void)file;
-	(void)offset;
-	(void)orig;
-	return -EINVAL;
+        loff_t ret;
+        struct inode *inode = file->f_mapping->host;
+
+        mutex_lock(&inode->i_mutex);
+        switch (orig) {
+        case 0:
+                file->f_pos = offset;
+                ret = file->f_pos;
+                break;
+        case 1:
+                file->f_pos += offset;
+                ret = file->f_pos;
+                break;
+        default:
+                ret = -EINVAL;
+        }   
+        mutex_unlock(&inode->i_mutex);
+        return ret;
 }
+
 
 static ssize_t msr_read(struct file *file, char __user *buf,
 			size_t count, loff_t *ppos)
