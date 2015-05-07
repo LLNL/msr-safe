f98ed17afd6f0793b72539e54b7ee71dbf4c8a38
Barry <rountree4@llnl.gov>
2015-05-06 22:28:31 -0700

    35_18.h

---
+++ whitelists/table_35_18.h
@@ -0,0 +1,11 @@
+// table_35_18.h
+#ifndef __TABLE_35_18_H
+#define __TABLE_35_18_H
+/* Table 35-18 : 3rd Generation Intel Core Processor Family
+ * (Based on Ivy Bridge Microarchitecture)
+ *
+ *
+ */
+
+//NO ADDITIONAL REGISTERS THAT WE USE IN THIS TABLE YET
+#endif //__TABLE_35_18_H
