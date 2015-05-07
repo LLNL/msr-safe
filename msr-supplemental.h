93b7e75cc866534204a86249ef2dd545150fddde
Barry <rountree4@llnl.gov>
2015-05-07 09:24:35 -0700

    Last of the compile-time bugfixes.
    Compiles without error or warning now.

--- msr-supplemental.h
+++ msr-supplemental.h
@@ -21,43 +21,38 @@
 
 // Each table_*.h file defines one WL_TABLE.
 
-//#include "table_35_02.h" usued. 
-#include "./whitelists/table_35_17.h"
-#include "./whitelists/table_35_20.h"
-#include "./whitelists/table_35_23.h"
+//#include "table_35_02.h" unused.  
 #include "./whitelists/table_35_15.h"
-#include "./whitelists/table_35_18.h"
-//#include "table_35_21.h" conflict; talk to Kathleen.
-#include "./whitelists/table_35_24.h"
 #include "./whitelists/table_35_16.h"
+#include "./whitelists/table_35_17.h"
+//#include "./whitelists/table_35_18.h" currently empty
 #include "./whitelists/table_35_19.h"
-#include "./whitelists/table_35_22.h"
-
-
-#ifdef _USE_ARCH_063F
-#define _USE_TABLE_35_24
-#define SMSR_063F ENTRY1 ENTRY_END
-//Table35_24 does not have a WL_ defined
-//INCOMPLETE because for next gen Haswell
-#endif // _USE_ARCH_063F
+//#include "./whitelists/table_35_20.h" currently empty
+//#include "table_35_21.h" conflict; talk to Kathleen.
+//#include "./whitelists/table_35_22.h" currently empty
+//#include "./whitelists/table_35_23.h" currently empty
 
 #ifdef _USE_ARCH_0646		//Haswell
 // Temporarily removing WL_TABLE_35_21 because matches PEFVEVTSEL in WL_TABLE_35_15
-#define SMSR_0646 WL_TABLE_35_15 WL_TABLE_35_16 WL_TABLE_35_18 WL_TABLE_35_22 WL_TABLE_35_23
+#define SMSR_0646 WL_TABLE_35_15 WL_TABLE_35_16 
+//WL_TABLE_35_18 WL_TABLE_35_22 WL_TABLE_35_23
 #endif // _USE_ARCH_0646
 
 #ifdef _USE_ARCH_0645		//Haswell
 // Temporarily removing WL_TABLE_35_21 because matches PEFVEVTSEL in WL_TABLE_35_15
-#define SMSR_0645 WL_TABLE_35_15 WL_TABLE_35_16 WL_TABLE_35_18 WL_TABLE_38_23
+#define SMSR_0645 WL_TABLE_35_15 WL_TABLE_35_16 
+//WL_TABLE_35_18 WL_TABLE_35_23
 #endif // _USE_ARCH_0645
 
 #ifdef _USE_ARCH_063C		//Haswell
 // Temporarily removing WL_TABLE_35_21 because matches PEFVEVTSEL in WL_TABLE_35_15
-#define SMSR_063C WL_TABLE_35_15 WL_TABLE_35_16 WL_TABLE_35_18 WL_TABLE_38_23
+#define SMSR_063C WL_TABLE_35_15 WL_TABLE_35_16 
+//WL_TABLE_35_18 WL_TABLE_35_23
 #endif // _USE_ARCH_063C
 
 #ifdef _USE_ARCH_063E		//Ivy Bridge
-#define SMSR_063E WL_TABLE_35_19 WL_TABLE_35_20 WL_TABLE_35_15
+#define SMSR_063E WL_TABLE_35_15 WL_TABLE_35_19 
+//WL_TABLE_35_20 
 #endif // _USE_ARCH_063E
 
 #ifdef _USE_ARCH_062D		//Sandy Bridge 
