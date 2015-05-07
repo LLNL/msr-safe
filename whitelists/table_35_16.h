d09f90f32f09bc86aaabdc537aead0d73731ec93
Barry <rountree4@llnl.gov>
2015-05-06 22:33:56 -0700

    35_16.h

---
+++ whitelists/table_35_16.h
@@ -0,0 +1,15 @@
+// table_35_16.h
+#ifndef __TABLE_35_16_H
+#define __TABLE_35_16_H
+
+#ifdef _USE_TABLE_35_16
+/* Table 35-16 : 2nd Generation Intel Core Processor Family (Based on Intel
+ * Microarchitecture Code Name Sandy Bridge)
+ *
+ * These are specific to the 2nd generation with CPUID signature 06_2AH
+ *
+ */
+#define WH_TABLE_35_16 \
+SMSR_ENTRY( SMSR_TURBO_RATIO_LIMIT,     0x1AD, 0xFFFFFFFF, 0xFFFFFFFF),
+
+#endif //__TABLE_35_16_H
