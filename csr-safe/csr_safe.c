bd4b96789347624d65776b359b12c5a4f17479b2
Scott Walker <walker91@llnl.gov>
2016-04-08 18:08:49 -0700

    added copyright information, added haswell whitelist

--- csr-safe/csr_safe.c
+++ csr-safe/csr_safe.c
@@ -1,6 +1,34 @@
-// Author: Scott Walker
-// csr_entry.c
-// Kernel module to provide safe access to pcie config space
+/* csr_entry.c
+ *
+ * Copyright (c) 2015-2016, Lawrence Livermore National Security, LLC. LLNL-CODE-645430
+ * Produced at Lawrence Livermore National Laboratory  
+ * Written by  Barry Rountree, rountree@llnl.gov
+ *             Scott Walker,   walker91@llnl.gov
+ *             Marty McFadden, mcfadden8@llnl.gov
+ *			   Kathleen Shoga, shoga1@llnl.gov
+ *
+ * All rights reserved. 
+ * 
+ * This file is part of msr-safe.
+ * 
+ * msr-safe is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * msr-safe is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with msr-safe.  If not, see <http://www.gnu.org/licenses/>. 
+ *
+ * This material is based upon work supported by the U.S. Department
+ * of Energy's Lawrence Livermore National Laboratory. Office of
+ * Science, under Award number DE-AC52-07NA27344.
+ *
+ */
 
 #include <linux/pci.h>
 #include <linux/module.h>
--- csr-safe/csr_whitelist.h
+++ csr-safe/csr_whitelist.h
@@ -1,6 +1,34 @@
-// csr_whitelist.h
-// Author: Scott Walker
-// Description: whitelist for csr access
+/* csr_whitelist.h
+ *
+ * Copyright (c) 2015-2016, Lawrence Livermore National Security, LLC. LLNL-CODE-645430
+ * Produced at Lawrence Livermore National Laboratory  
+ * Written by  Barry Rountree, rountree@llnl.gov
+ *             Scott Walker,   walker91@llnl.gov
+ *             Marty McFadden, mcfadden8@llnl.gov
+ *			   Kathleen Shoga, shoga1@llnl.gov
+ *
+ * All rights reserved. 
+ * 
+ * This file is part of msr-safe.
+ * 
+ * msr-safe is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * msr-safe is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with msr-safe.  If not, see <http://www.gnu.org/licenses/>. 
+ *
+ * This material is based upon work supported by the U.S. Department
+ * of Energy's Lawrence Livermore National Laboratory. Office of
+ * Science, under Award number DE-AC52-07NA27344.
+ *
+ */
 
 #ifndef _ARCH_X86_KERNEL_CSR_WHITELIST_H
 #define _ARCH_X86_KERNEL_CSR_WHITELIST_H 1
