97deaca05bae155b04973628430bbcfc2a6ab244
Stephanie Labasan <labasan1@llnl.gov>
2017-01-11 17:30:02 -0800

    Whitelisting MSR_MISC_FEATURE_CONTROL for Knights Landing. This fixes #20.   - Updated RPM install to use Knights Landing whitelist.
    Signed-off-by: Stephanie Labasan <labasan1@llnl.gov>

--- msr-smp.c
+++ msr-smp.c
@@ -42,50 +42,53 @@
 
 static void __msr_safe_batch(void *info)
 {
-	struct msr_batch_array *oa = info;
-	struct msr_batch_op *op;
-	int this_cpu = smp_processor_id();
-	u32 *dp;
-	u64 oldmsr;
-	u64 newmsr;
+    struct msr_batch_array *oa = info;
+    struct msr_batch_op *op;
+    int this_cpu = smp_processor_id();
+    u32 *dp;
+    u64 oldmsr;
+    u64 newmsr;
 
-	for (op = oa->ops; op < oa->ops + oa->numops; ++op) {
-		if (op->cpu != this_cpu)
-			continue;
+    for (op = oa->ops; op < oa->ops + oa->numops; ++op)
+    {
+        if (op->cpu != this_cpu)
+            continue;
 
-		op->err = 0;
-		dp = (u32 *)&oldmsr;
-		if (rdmsr_safe(op->msr, &dp[0], &dp[1])) {
-			op->err = -EIO;
-			continue;
-		}
-		if (op->isrdmsr) {
-			op->msrdata = oldmsr;
-			continue;
-		}
+        op->err = 0;
+        dp = (u32 *)&oldmsr;
+        if (rdmsr_safe(op->msr, &dp[0], &dp[1]))
+        {
+            op->err = -EIO;
+            continue;
+        }
+        if (op->isrdmsr)
+        {
+            op->msrdata = oldmsr;
+            continue;
+        }
 
-		newmsr = op->msrdata & op->wmask;
-		newmsr |= (oldmsr & ~op->wmask);
-		dp = (u32 *)&newmsr;
-		if (wrmsr_safe(op->msr, dp[0], dp[1]))
-			op->err = -EIO;
-	}
+        newmsr = op->msrdata & op->wmask;
+        newmsr |= (oldmsr & ~op->wmask);
+        dp = (u32 *)&newmsr;
+        if (wrmsr_safe(op->msr, dp[0], dp[1]))
+            op->err = -EIO;
+    }
 }
 
 int msr_safe_batch(struct msr_batch_array *oa)
 {
-	struct cpumask cpus_to_run_on;
-	struct msr_batch_op *op;
+    struct cpumask cpus_to_run_on;
+    struct msr_batch_op *op;
 
-	cpumask_clear(&cpus_to_run_on);
-	for (op = oa->ops; op < oa->ops + oa->numops; ++op)
-		cpumask_set_cpu(op->cpu, &cpus_to_run_on);
+    cpumask_clear(&cpus_to_run_on);
+    for (op = oa->ops; op < oa->ops + oa->numops; ++op)
+        cpumask_set_cpu(op->cpu, &cpus_to_run_on);
 
-	on_each_cpu_mask(&cpus_to_run_on, __msr_safe_batch, oa, 1);
+    on_each_cpu_mask(&cpus_to_run_on, __msr_safe_batch, oa, 1);
 
-	for (op = oa->ops; op < oa->ops + oa->numops; ++op)
-		if (op->err)
-			return op->err;
+    for (op = oa->ops; op < oa->ops + oa->numops; ++op)
+        if (op->err)
+            return op->err;
 
-	return 0;
+    return 0;
 }
