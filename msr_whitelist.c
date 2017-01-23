7709fdeeac0a68215f32ece3e434590469942e9b
Marty McFadden <mcfadden8@llnl.gov>
2017-01-23 03:12:51 +0000

    - Checking ZERO_SIZE_PTR() instead of NULL for allocation rvals - No longer performing 2nd pass if no entries were parsed during the first pass   of reading in the whitelist entries - Removed old *msrdata element from msr_whitelist_entry structure since it is   no longer being used - Added mechanism to enable debug information from driver when uncommented. - Log an error message to console if invalid whitelist provided.
    Signed-off-by: Marty McFadden <mcfadden8@llnl.gov>

--- msr_whitelist.c
+++ msr_whitelist.c
@@ -47,8 +47,15 @@
 struct whitelist_entry {
 	u64 wmask;	/* Bits that may be written */
 	u64 msr;	/* Address of msr (used as hash key) */
-	u64 *msrdata;	/* ptr to original msr contents of writable bits */
 	struct hlist_node hlist;
+};
+
+struct whitelist_data {
+	u32 entries;	/* WL entries successfully parsed */
+	int ecode;	/* 0 if things look good */
+	char *partial_line;
+	int partial_len;	/* Length of partial line */
+	struct whitelist_entry *whitelist;
 };
 
 static void delete_whitelist(void);
@@ -114,6 +121,7 @@ static ssize_t write_whitelist(struct file *file, const char __user *buf,
 	struct whitelist_entry *entry;
 	char *kbuf;
 
+	pr_debug("%s: %p, %zu, %llu\n", __FUNCTION__, buf, count, *ppos);
 	if (count <= 2) {
 		mutex_lock(&whitelist_mutex);
 		delete_whitelist();
@@ -123,16 +131,21 @@ static ssize_t write_whitelist(struct file *file, const char __user *buf,
 	}
 
 	if (count+1 > MAX_WLIST_BSIZE) {
-		pr_debug("write_whitelist: buffer of %zu bytes too large\n",
-		    count);
+		pr_err("%s: buffer of %zu bytes too large\n",
+		    __FUNCTION__, count);
 		return -EINVAL;
 	}
 
 	kbuf = kzalloc(count+1, GFP_KERNEL);
-	if (!kbuf)
+	if (ZERO_OR_NULL_PTR(kbuf)) {
+		pr_err("%s: memory alloc(%zu) failed\n",
+			__FUNCTION__, count+1);
 		return -ENOMEM;
+	}
 
 	if (copy_from_user(kbuf, tmp, count)) {
+		pr_err("%s: copy_from_user(%zu) failed\n",
+			__FUNCTION__, count);
 		err = -EFAULT;
 		goto out_freebuffer;
 	}
@@ -141,6 +154,7 @@ static ssize_t write_whitelist(struct file *file, const char __user *buf,
 	for (num_entries = 0, s = kbuf, res = 1; res > 0; ) {
 		res = parse_next_whitelist_entry(s, &s, 0);
 		if (res < 0) {
+			pr_err("%s: parse error\n", __FUNCTION__);
 			err = res;
 			goto out_freebuffer;
 		}
@@ -149,10 +163,19 @@ static ssize_t write_whitelist(struct file *file, const char __user *buf,
 			num_entries++;
 	}
 
+	if (num_entries == 0) {
+		pr_err("%s: No valid entries found in %zu bytes of input\n",
+			__FUNCTION__, count);
+		err = -EINVAL;
+		goto out_freebuffer;
+	}
+
 	/* Pass 2: */
 	mutex_lock(&whitelist_mutex);
 	res = create_whitelist(num_entries);
 	if (res < 0) {
+		pr_err("%s: create_whitelist(%d) failed\n",
+			__FUNCTION__, num_entries);
 		err = res;
 		goto out_releasemutex;
 	}
@@ -160,7 +183,7 @@ static ssize_t write_whitelist(struct file *file, const char __user *buf,
 	for (entry = whitelist, s = kbuf, res = 1; res > 0; entry++) {
 		res = parse_next_whitelist_entry(s, &s, entry);
 		if (res < 0) {
-			pr_debug("write_whitelist: Table corrupted\n");
+			pr_err("%s: Table corrupted\n", __FUNCTION__);
 			delete_whitelist();
 			err = res; /* This should not happen! */
 			goto out_releasemutex;
@@ -168,8 +191,8 @@ static ssize_t write_whitelist(struct file *file, const char __user *buf,
 
 		if (res) {
 			if (find_in_whitelist(entry->msr)) {
-				pr_debug("write_whitelist: Duplicate: %llx\n",
-							 entry->msr);
+				pr_err("%s: Duplicate: %llx\n",
+						 __FUNCTION__, entry->msr);
 				err = -EINVAL;
 				delete_whitelist();
 				goto out_releasemutex;
@@ -233,11 +256,8 @@ static const struct file_operations fops = {
 
 static void delete_whitelist(void)
 {
-	if (whitelist == 0)
+	if (ZERO_OR_NULL_PTR(whitelist))
 		return;
-
-	if (whitelist->msrdata != 0)
-		kfree(whitelist->msrdata);
 
 	kfree(whitelist);
 	whitelist = 0;
@@ -251,7 +271,7 @@ static int create_whitelist(int nentries)
 	whitelist_numentries = nentries;
 	whitelist = kcalloc(nentries, sizeof(*whitelist), GFP_KERNEL);
 
-	if (!whitelist)
+	if (ZERO_OR_NULL_PTR(whitelist))
 		return -ENOMEM;
 	return 0;
 }
@@ -309,7 +329,7 @@ static int parse_next_whitelist_entry(char *inbuf, char **nextinbuf,
 			s++;
 
 		if (*s == 0) {
-			pr_debug("parse_next_whitelist_entry: Premature EOF");
+			pr_debug("%s: Premature EOF\n", __FUNCTION__);
 			return -EINVAL;
 		}
 
@@ -366,7 +386,7 @@ int msr_whitelist_init(void)
 
 	majordev = register_chrdev(0, "cpu/msr_whitelist", &fops);
 	if (majordev < 0) {
-		pr_debug("msr_whitelist_init: unable to register chrdev\n");
+		pr_debug("%s: unable to register chrdev\n", __FUNCTION__);
 		msr_whitelist_cleanup();
 		return -EBUSY;
 	}
