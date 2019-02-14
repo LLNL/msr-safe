ff888c9368b67473737d3fbc8b0b4f6ebe68d8ca
Diana Guttman <diana.r.guttman@intel.com>
2019-02-14 13:55:23 -0800

    Modify msrsave and msrrestore functions to write to *FILE rather than stdout and stderr.
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- msrsave/msrsave.h
+++ msrsave/msrsave.h
@@ -33,14 +33,20 @@
 #ifndef MSRSAVE_H_INCLUDE
 #define MSRSAVE_H_INCLUDE
 
+#include <stdio.h>
+
 int msr_save(const char *out_path,
              const char *whitelist_path,
              const char *msr_path,
-             int num_cpu);
+             int num_cpu,
+             FILE *output_log,
+             FILE *error_log);
 
 int msr_restore(const char *in_path,
                 const char *whitelist_path,
                 const char *msr_path,
-                int num_cpu);
+                int num_cpu,
+                FILE *output_log,
+                FILE *error_log);
 
 #endif
