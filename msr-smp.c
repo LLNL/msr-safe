fc21e7eccc04d38523d8d34079500d08ec31c260
Barry <rountree4@llnl.gov>
2022-10-02 11:38:23 -0700

    Adds ENOMEM check to zalloc_cpumask_var call
    Typo fix.

--- msr-smp.c
+++ msr-smp.c
@@ -65,7 +65,9 @@ int msr_safe_batch(struct msr_batch_array *oa)
     cpumask_var_t cpus_to_run_on;
     struct msr_batch_op *op;
 
-    zalloc_cpumask_var(&cpus_to_run_on, GFP_KERNEL);
+    if(!zalloc_cpumask_var(&cpus_to_run_on, GFP_KERNEL)){
+        return -ENOMEM;
+    }
 
     for (op = oa->ops; op < oa->ops + oa->numops; ++op)
     {
